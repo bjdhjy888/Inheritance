ce78ded5b99d3fc2198f325b373015d215ce2255
Bob <41116033+bjdhjy888@users.noreply.github.com>
2020-06-27 23:18:58 +0800

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,13 @@
+//Class: for creating objects.
+//Doctor is a class.
+//See also: https://www.youtube.com/watch?v=FAYcq_5vP8k
+
+package com.company;
+
+public class Doctor {
+    public void CheckHealth(int HeartRateOfPresident){
+        if(HeartRateOfPresident > 120){
+            System.out.println("You are sick.");
+        }
+    }
+}
